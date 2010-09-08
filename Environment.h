31520e4b58f48a7382307a5a29ef0cfa4c58d95c
Tim Lovorn <tflovorn@crimson.ua.edu>
2010-09-08 13:44:15 -0500

    oops. apparently git add needs to run just before committing. now Environment.h is really here.

--- Environment.h
+++ Environment.h
@@ -1,3 +1,18 @@
 class Environment {
-
+public:
+    // Construct an Environment from configuration data.
+    Environment(const ConfigData& cfg);
+    // Brillouin zone length (number of points = grid length ^ 2).
+    const double gridLen;
+    // Physical parameters.
+    const double t0,    // Overall energy scale (default = 1.0).
+                 tz,    // z-direction hopping energy.
+                 thp,   // Diagonal (next-nearest-neighbor) hopping energy.
+                 x,     // Doping / holon excess.
+                 th,    // One-holon hopping energy: th = t0 * (1 - x).
+                 alpha; // alpha = {-1, 1} -> {d-wave, s-wave} symmetry
+    // Initial conditions.
+    const double initD1, initMu, initF0;
+    // Tolerances.
+    const double tolD1, tolMu, tolF0;
 };
