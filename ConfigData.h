1d5ad1a0457e3aec653e4cff2fd4104f667851b6
Tim Lovorn <tflovorn@crimson.ua.edu>
2010-09-08 14:03:35 -0500

    Configuration has arrived...

---
+++ ConfigData.h
@@ -0,0 +1,17 @@
+class ConfigData {
+public:
+    // Constructor will trigger reading.
+    ConfigData(std::ifstream infs);
+    // Destructor needed to close file.
+    ~ConfigData();
+    // Parameters for Environment to scoop up (descriptions in Environment.h)
+    // We ASSUME these don't get messed with after reading.  Dangerous.
+    std::string label;
+    double gridLen, t0, tz, thp, x, th, alpha, initD1, initMu, initF0,
+           tolD1, tolMu, tolF0;
+private:
+    // Input file stream, specified at construction.
+    std::ifstream myInput;
+    // Read data from myInput and store it.
+    void readInputFile();
+};
