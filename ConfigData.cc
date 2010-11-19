3dc0ecdaaf6c4dffac2ec7dd01ef7c4a4906d3fb
Tim Lovorn <tflovorn@crimson.ua.edu>
2010-11-19 17:11:13 -0600

    removed debug printing from ConfigData

--- ConfigData.cc
+++ ConfigData.cc
@@ -19,8 +19,6 @@ void ConfigData::readFromFile(const std::string& cfgFileName) {
             key = line.substr(0, split);
             value = line.substr(split + 1, line.length() - split - 1);
             setValue(key, value);
-            std::cout << "adding key = " << key 
-                << ", value = " << value << std::endl;
         }
     }
     delete lines;
