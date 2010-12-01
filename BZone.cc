1c5adbfcad6ee8b0379bf61554fda36250e4cff4
Tim Lovorn <tflovorn@crimson.ua.edu>
2010-11-30 19:11:41 -0600

    added GPLv3 license

--- BZone.cc
+++ BZone.cc
@@ -1,10 +1,27 @@
+/****************************************************************************
+ * Copyright (C) 2010 Tim Lovorn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
 #include <cmath>
 #include <cfloat>
 
 #include "BZone.hh"
 
-// Would be nice to have a single function handle transforming one BZone point into another
-// instead of duplicating the traversal
+// Would be nice to have a single function handle transforming one BZone point
+// into another instead of duplicating the traversal
 
 double BZone::minimum(const State& st, bzFunction func) {
     double kx = -M_PI, ky = -M_PI, min = DBL_MAX, val;
