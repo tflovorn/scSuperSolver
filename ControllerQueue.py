add9eb3d82f4fb135ce02bb0b4c59bc39d04400f
Tim Lovorn <tflovorn@crimson.ua.edu>
2010-12-06 15:57:33 -0600

    wrote basic ControllerQueue, untests

--- ControllerQueue.py
+++ ControllerQueue.py
@@ -15,8 +15,30 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
 
-import FileDict
+import os
 
-myDict = FileDict.FileDict("test_FileDict.fd")
-print myDict.topDict
-myDict.writeToFile("test_FileDict_rewrite.fd")
+CQ_MAX_PROCESSES = 2
+
+class ControllerQueue(object):
+    def __init__(self, initialQueue=None, maxProcesses=CQ_MAX_PROCESS):
+        self.maxProcesses = maxProcesses
+        if initialQueue == None:
+            self.queue = []
+        else:
+            self.queue = initialQueue
+
+    def enqueue(self, newItem):
+        self.queue.append(newItem)
+
+    def enqueueList(self, newItemList):
+        self.queue.extend(newItemList)
+
+    def runAll(self):
+        pids = []
+        while len(self.queue) > 0:
+            pid = os.spawnl(os.P_NOWAIT, "mainController.out", self.queue[0])
+            pids.append(pid)
+            self.queue = self.queue[1:]
+            while len(pids) >= self.maxProcesses:
+                os.waitpid(pids[0], 0)
+                pids = pids[1:]
