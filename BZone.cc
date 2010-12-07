b03ce7c836a55d5c117d9b752f162ba1259d9a2c
Tim Lovorn <tflovorn@crimson.ua.edu>
2010-12-07 13:14:43 -0600

    should probably use my full name for the copyright notice >.<

--- BZone.cc
+++ BZone.cc
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright (C) 2010 Tim Lovorn
+ * Copyright (C) 2010 Timothy Lovorn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
